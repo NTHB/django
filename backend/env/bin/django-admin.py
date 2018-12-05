68230f21cd104879f91a96949f7b7d9eb2fc149c
noppawit <noppawit.han@gmail.com>
2018-12-05 01:29:56 -0800

    delete commit error

--- backend/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Noppawit/Desktop/Directed Study/journal_app/backend/env/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
